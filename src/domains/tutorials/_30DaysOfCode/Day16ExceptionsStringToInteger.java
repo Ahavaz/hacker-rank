39ab74b114272d8599301633d119a9aac02a94f1
Lucas César <Ahavaz@users.noreply.github.com>
2016-04-19 07:35:28 -0300

    Solved Day16 exercise

--- src/domains/tutorials/_30DaysOfCode/Day16ExceptionsStringToInteger.java
+++ src/domains/tutorials/_30DaysOfCode/Day16ExceptionsStringToInteger.java
@@ -1,7 +1,18 @@
 package domains.tutorials._30DaysOfCode;
 
+import java.util.Scanner;
+
 public class Day16ExceptionsStringToInteger {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		in.close();
 
+		try {
+			int num = Integer.parseInt(s);
+			System.out.println(num);
+		} catch (NumberFormatException e) {
+			System.out.println("Bad String");
+		}
 	}
 }
