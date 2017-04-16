7a0a8276b26c0042d0531aee62857820fd3ffaa4
Lucas César <Ahavaz@users.noreply.github.com>
2017-04-15 22:46:22 -0300

    Update Solution.java

--- src/domains/tutorials/_30_days_of_code/day_08_dictionaries_and_maps/Solution.java
+++ src/domains/tutorials/_30_days_of_code/day_08_dictionaries_and_maps/Solution.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] argh) {
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		Map<String, String> phoneBook = new HashMap<String, String>();
