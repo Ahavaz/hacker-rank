0e115753241cd7d37b99d00de7a40f67b4846b10
Lucas César <Ahavaz@users.noreply.github.com>
2017-04-15 22:31:05 -0300

    Create Solution.java

---
+++ src/domains/tutorials/cracking_the_coding_interview/arrays_left_rotation/Solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int k = in.nextInt();
+    int a[] = new int[n];
+    int b[] = new int[n];
+    int t = 0;
+    for(int i=0; i < n; i++){
+      a[i] = in.nextInt();
+    }
+
+    while(k-- > 0){
+      t = a[0];
+      for(int i=0; i < n-1; i++){
+        a[i] = a[i+1];
+      }
+      a[n-1] = t;
+    }
+    
+    for(int num : a){
+      System.out.print(num + " ");
+    }
+  }
+}
